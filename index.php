f4c36b70fa8f7076f5a97bd5a2b886ac26926994
Adekekaz <33420356+Adekekaz@users.noreply.github.com>
2024-05-21 22:10:39 +0600

    salemalem

--- index.php
+++ index.php
@@ -1,13 +1,15 @@
 <?php
-$person = [
-    'name' => 'vasya',
-    'age' => 28
-];
-
-print_r($person);
-
-$qwe = 'hghhg';
-
-if ($qwe) {
-    echo 'yes';
-};
+    $ip = $_SERVER['REMOTE_ADDR'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Gitpen jumis</title>
+</head>
+<body>
+    <h1>Salem, alem!</h1>
+    <h2>Ваш IP-адрес: <?= $ip; ?>. </h2>
+</body>
+</html>
